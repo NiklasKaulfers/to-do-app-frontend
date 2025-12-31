dd48ec17f8102946ee7faaba31dbe5a9b4748f3a
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2025-12-31 14:30:56 +0100

    Adding creation of lists

--- src/components/lists/create-list.tsx
+++ src/components/lists/create-list.tsx
@@ -1,8 +1,123 @@
+import {type FormEvent, useState} from "react";
+import {useMediaQuery} from "@mui/system";
+import * as uuid from "uuid";
+import AuthHandler from "../../services/auth-service.tsx";
+import AuthError from "../../errors/auth-error.tsx";
+import {Box, Button, TextField, Typography} from "@mui/material";
 
 export default function CreateList() {
+    const [title, setTitle] = useState("");
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState(false);
+    useMediaQuery("(prefers-color-scheme: dark )")
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+
+        const id: string = uuid.v6();
+
+        const idToken: string | undefined = AuthHandler.getAuthContext().user?.id_token;
+
+        if (!idToken) throw new AuthError("Invalid Auth Token");
+
+        setLoading(true);
+        setError("");
+        setSuccess(false);
+
+        try {
+            const response: Response = await fetch(import.meta.env.VITE_API_URL! + "/List", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": idToken
+                },
+                body: JSON.stringify({
+                    list: id,
+                    name: title,
+                }),
+            });
+            console.dir(response);
+            if (!response.ok) {
+                const message = await response.text();
+                throw new Error(message || `Request failed (${response.status})`);
+            }
+
+            setSuccess(true);
+            setTitle("");
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                setError(error.message)
+            } else {
+                setError("Something went wrong")
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <h1>
-            Not implemented
-        </h1>
-    )
+        <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{maxWidth: 400, mx: "auto", mt: 4}}>
+            <Typography
+                variant="h4"
+                content="create a new list"
+            >
+                create a new list
+            </Typography>
+            <TextField
+                label="Name"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+                fullWidth
+                margin="normal"
+            />
+
+            <Button
+                type="submit"
+                variant="contained"
+                sx={{mt: 2}}
+                disabled={!title}
+            >
+                Submit
+            </Button>
+            {
+                success && (
+                    <Typography
+                        variant="body1"
+                        component="p"
+                        className="text-green-500"
+                    >
+                        successfully created list
+                    </Typography>
+                )
+            }
+            {
+                error && (
+                    <Typography
+                        variant="body1"
+                        component="p"
+                        className="text-red-600"
+                    >
+                        an error happened during creation
+                    </Typography>
+                )
+            }
+            {
+                loading && (
+                    <Typography
+                        variant="body1"
+                        component="p"
+                        className="text-orange-600"
+                    >
+                        loading...
+                    </Typography>
+                )
+            }
+        </Box>
+    );
 }
--- src/components/lists/list-card.tsx
+++ src/components/lists/list-card.tsx
@@ -1,11 +1,13 @@
 import {Button, Card, CardContent, Typography} from "@mui/material";
 import type {GetListsResponse} from "../../services/api/lists.tsx";
+import {useNavigate} from "react-router-dom";
 
 interface ListCardProps {
     list?: GetListsResponse
 }
 
-export default function ListCard({list}: ListCardProps) {
+export default function ListCard({list}: Readonly<ListCardProps>) {
+    const navigate = useNavigate();
     return (
         <Card>
             <CardContent>
@@ -19,6 +21,21 @@ export default function ListCard({list}: ListCardProps) {
             <Button variant="contained" color="secondary" href={`/lists/` + list?.id}>
                 go to {list?.name}
             </Button>
+                {
+                    (list?.inLists) &&
+                    <CardContent>
+                        {list.inLists.map((listId: string) => {
+                            return (
+                                <Typography
+                                    variant="body1"
+                                    onClick={ () => navigate("/todos/" + listId)}
+                                >
+                                    {listId}
+                                </Typography>
+                            )
+                        })}
+                    </CardContent>
+                }
         </Card>
     )
 }
